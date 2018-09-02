ad93e044881058e3b73ca7fa95020c3e3610214a
jonathanASC4 <jonathanrgreco@gmail.com>
2018-09-02 12:28:49 -0400

    API works, kinda

--- src/pages/challenge.js
+++ src/pages/challenge.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import FormControlLabel from '@material-ui/core/FormControlLabel';
 import { Link } from 'react-router-dom';
 import Button from '@material-ui/core/Button';
 import Typography from '@material-ui/core/Typography';
@@ -10,11 +11,16 @@ import Footer from '../components/footer.js';
 import Grow from '@material-ui/core/Grow';
 import Avatar from '@material-ui/core/Avatar';
 
+const proxyURL = "https://stark-beyond-49493.herokuapp.com/";
+const URL1 = "https://www.politifact.com/api/statements/truth-o-meter/people/barack-obama/json/?n=100";
+
 class Claim extends React.Component{
     constructor(){
         super();
         this.state = {
-            pictures: [],
+            statements: ["a", "a", "a", "a", "a", "a", "a", "a", "a", "a"],
+            claim1: <p>give us a moment</p>,
+            text: <p>getting desperate</p>
         }
         console.log("checking");
     }
@@ -25,38 +31,40 @@ class Claim extends React.Component{
     //https://randomuser.me/api/?results=500
     //https://www.politifact.com/api/statements/truth-o-meter/people/barack-obama/json/?n=100
     //http://politifact.com/api/v/2/statement/?format=[JSON]&order_by=-ruling_date&limit=1
+    //https://stark-beyond-49493.herokuapp.com/
     componentDidMount(){
-        fetch("https://www.politifact.com/api/statements/truth-o-meter/people/barack-obama/json/?n=100", {'mode': 'no-cors'})
+        fetch("https://cors-anywhere.herokuapp.com/"+URL1)
         .then(results => {
-            //return results.json();
-        })
-        /*
-        .then(results => {
+            //console.log(results.json());
             return results.json();
-        }).then(data => {
-            let pictures = data.results.map((pic => {
-                return(
-                    <div key={pic.results}>
-                        <img src={pic.picture.medium} />
-                    </div>
-                )
-            }))
-            this.setState({pictures: pictures});
-            console.log("state", this.state.pictures);
         })
-        */
-        //     data=data.filter(function(item){return item.speaker.name_slug=="barack-obama" && item.statement_type.statement_type !== "Flip";});
-        // }).then(questions => {
-        //     this.setState({
-        //         statements: [questions[0]],
-        //     });
-        // })
+        .then(data => {
+            data=data.filter(function(item){return item.speaker.name_slug=="barack-obama" && item.statement_type.statement_type !== "Flip";});
+            var arr = [];
+            for(var i=0; i<10; i++){
+                arr.push(data.splice(Math.random()*data.length, 1)[0]);
+            }
+            this.setState({
+                statements: arr,
+                claim1: arr[0].statement,
+            });
+            console.log(this.state.statements);
+            console.log(arr[0].statement);
+        })
+        
     }
     render(){
+        if(this.state.claim1 !== undefined && this.state.text == <p>getting desperate</p>){
+            console.log(this.state.claim1);
+            this.setState({
+                text: this.state.claim1,
+            })
+        }
         return(
             <div className="container2">
                 <div className="container1">
-                    {this.state.pictures}
+                    
+                    <FormControlLabel control={<div>{this.state.claim1}</div>} />
                 </div>
             </div>
         )
