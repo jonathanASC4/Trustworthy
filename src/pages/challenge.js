3cfb4b270700ff273cc0fd541a39f049855495ad
jonathanASC4 <jonathanrgreco@gmail.com>
2018-09-03 15:02:49 -0400

    site now pulls statements made by multiple people

--- src/pages/challenge.js
+++ src/pages/challenge.js
@@ -13,7 +13,10 @@ import Grow from '@material-ui/core/Grow';
 import Avatar from '@material-ui/core/Avatar';
 
 const proxyURL = "https://stark-beyond-49493.herokuapp.com/";
-const URL1 = "https://www.politifact.com/api/statements/truth-o-meter/people/barack-obama/json/?n=100";
+const currentProxy = "https://cors-anywhere.herokuapp.com/";
+const URL1 = "https://www.politifact.com/api/statements/truth-o-meter/people/barack-obama/json/?n=50";
+const URL2 = "https://www.politifact.com/api/statements/truth-o-meter/people/hillary-clinton/json/?n=210";
+const URL3 = "https://www.politifact.com/api/statements/truth-o-meter/people/bernie-s/json/?n=70";
 
 class Claim extends React.Component{
     constructor(){
@@ -33,34 +36,83 @@ class Claim extends React.Component{
     //http://politifact.com/api/v/2/statement/?format=[JSON]&order_by=-ruling_date&limit=1
     //https://stark-beyond-49493.herokuapp.com/
     componentDidMount(){
-        fetch("https://cors-anywhere.herokuapp.com/"+URL1)
+        var superArray = [];
+        var arr = [];
+        fetch(currentProxy+URL1)
         .then(results => {
-            //console.log(results.json());
             return results.json();
         })
         .then(data => {
+            
             data=data.filter(function(item){return item.speaker.name_slug=="barack-obama" && item.statement_type.statement_type !== "Flip";});
-            var arr = [];
             for(var i=0; i<10; i++){
-                arr.push(data.splice(Math.random()*data.length, 1)[0]);
+                arr.push(data.splice(Math.floor(Math.random()*data.length), 1)[0]);
             }
-            this.setState({
-                statements: arr,
-                claim1: arr[0].statement,
-            });
-            console.log(this.state.statements);
-            console.log(arr[0].statement);
+            
+            fetch(currentProxy+URL2)
+            .then(results2 => {
+                return results2.json();
+            })
+            .then(data2 => {
+                data2=data2.filter(function(item){return item.speaker.name_slug=="hillary-clinton" && item.statement_type.statement_type !== "Flip";});
+                for(var i=0; i<10; i++){
+                    arr.push(data2.splice(Math.floor(Math.random()*data2.length), 1)[0]);
+                }
+
+                fetch(currentProxy+URL3)
+                .then(results3 => {
+                    return results3.json();
+                })
+                .then(data3 => {
+                    data3=data3.filter(function(item){return item.speaker.name_slug=="bernie-s" && item.statement_type.statement_type !== "Flip";});
+                    for(var i=0; i<10; i++){
+                        arr.push(data3.splice(Math.floor(Math.random()*data3.length), 1)[0]);
+                    }
+
+                    this.setState({
+                        statements: arr,
+                        claim1: arr[Math.floor(Math.random()*arr.length)].statement,
+                    });
+                    console.log(this.state.statements);
+                    console.log(arr[0].statement);
+                })
+
+            })
+            
         })
         
+        // fetch("https://cors-anywhere.herokuapp.com/"+URL2)
+        // .then(results => {
+        //     return results.json();
+        // })
+        // .then(data => {
+        //     data=data.filter(function(item){return item.speaker.name_slug=="hillary-clinton" && item.statement_type.statement_type !== "Flip";});
+        //     for(var i=0; i<10; i++){
+        //         superArray.push(data.splice(Math.random()*data.length, 1)[0]);
+        //     }
+        // })
+        // fetch("https://cors-anywhere.herokuapp.com/"+URL3)
+        // .then(results => {
+        //     return results.json();
+        // })
+        // .then(data => {
+        //     data=data.filter(function(item){return item.speaker.name_slug=="bernie-s" && item.statement_type.statement_type !== "Flip";});
+        //     for(var i=0; i<10; i++){
+        //         superArray.push(data.splice(Math.random()*data.length, 1)[0]);
+        //     }
+        // })
+        // console.log(superArray);
+        // console.log(superArray[0]);
+        // this.setState({
+        //     statements: superArray,
+        //     claim1: superArray[Math.floor(Math.random()*superArray.length)],
+        // })
     }
     render(){
-        function createMarkup(){
-            return {__html: 'First &middot; Second'};
-        }
         return(
             <div className="container2">
                 <div className="container1">
-                    <FormControlLabel control={<div>{ReactHtmlParser(this.state.claim1)}</div>} />
+                    <div>{ReactHtmlParser(this.state.claim1)}</div>
                 </div>
             </div>
         )
