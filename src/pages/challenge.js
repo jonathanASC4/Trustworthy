9ba550d2b57b447c0213d1f3431de3946e503b53
jonathanASC4 <jonathanrgreco@gmail.com>
2018-09-04 11:33:33 -0400

    button displays Claim

--- src/pages/challenge.js
+++ src/pages/challenge.js
@@ -11,6 +11,7 @@ import NavBar from '../components/navbar.js';
 import Footer from '../components/footer.js';
 import Grow from '@material-ui/core/Grow';
 import Avatar from '@material-ui/core/Avatar';
+import TextField from '@material-ui/core/TextField';
 
 const proxyURL = "https://stark-beyond-49493.herokuapp.com/";
 const currentProxy = "https://cors-anywhere.herokuapp.com/";
@@ -18,15 +19,39 @@ const URL1 = "https://www.politifact.com/api/statements/truth-o-meter/people/bar
 const URL2 = "https://www.politifact.com/api/statements/truth-o-meter/people/hillary-clinton/json/?n=210";
 const URL3 = "https://www.politifact.com/api/statements/truth-o-meter/people/bernie-s/json/?n=70";
 
+const styles = theme => ({
+    multilineColor:{
+      color:'red',
+    },});
+
+var arr = [];
+
 class Claim extends React.Component{
     constructor(){
         super();
         this.state = {
             statements: ["a", "a", "a", "a", "a", "a", "a", "a", "a", "a"],
-            claim1: <p>give us a moment</p>,
+            attempt: [<p>I wanna try something</p>, <p>how does this work</p>],
+            testLength: 10,
+            test: <button onClick={() => this.displayClaims()}>do the thing</button>,
+            claim1: [],
         }
         console.log("checking");
     }
+
+    displayClaims(){
+        
+        this.setState({
+            test: this.state.claim1,
+        })
+        console.log(this.state.claim1);
+    }
+
+    handleChange(event, field){
+        this.setState({
+          [field]: event.target.value,
+        });
+      }
 
     speakerCheck(item, speaker_slug){
         return item.speaker.name_slug==speaker_slug && item.statement_type.statement_type !== "Flip";
@@ -36,8 +61,7 @@ class Claim extends React.Component{
     //http://politifact.com/api/v/2/statement/?format=[JSON]&order_by=-ruling_date&limit=1
     //https://stark-beyond-49493.herokuapp.com/
     componentDidMount(){
-        var superArray = [];
-        var arr = [];
+        
         fetch(currentProxy+URL1)
         .then(results => {
             return results.json();
@@ -69,50 +93,41 @@ class Claim extends React.Component{
                         arr.push(data3.splice(Math.floor(Math.random()*data3.length), 1)[0]);
                     }
 
+                    function returnPush(arr, val){
+                        var arr2 = JSON.parse(JSON.stringify(arr));
+                        arr2.push(val);
+                        return arr2;
+                    }
+
                     this.setState({
                         statements: arr,
-                        claim1: arr[Math.floor(Math.random()*arr.length)].statement,
+                        claim1: ReactHtmlParser(arr.splice(Math.floor(Math.random()*arr.length), 1)[0].statement),
+                        claim2: ReactHtmlParser(arr.splice(Math.floor(Math.random()*arr.length), 1)[0].statement),
+                        claim3: ReactHtmlParser(arr.splice(Math.floor(Math.random()*arr.length), 1)[0].statement),
+                        claim4: ReactHtmlParser(arr.splice(Math.floor(Math.random()*arr.length), 1)[0].statement),
+                        claim5: ReactHtmlParser(arr.splice(Math.floor(Math.random()*arr.length), 1)[0].statement),
+                        claim6: ReactHtmlParser(arr.splice(Math.floor(Math.random()*arr.length), 1)[0].statement),
+                        claim7: ReactHtmlParser(arr.splice(Math.floor(Math.random()*arr.length), 1)[0].statement),
+                        claim8: ReactHtmlParser(arr.splice(Math.floor(Math.random()*arr.length), 1)[0].statement),
+                        claim9: ReactHtmlParser(arr.splice(Math.floor(Math.random()*arr.length), 1)[0].statement),
+                        claim10: ReactHtmlParser(arr.splice(Math.floor(Math.random()*arr.length), 1)[0].statement),
                     });
-                    console.log(this.state.statements);
-                    console.log(arr[0].statement);
+                    console.log(arr.splice(Math.floor(Math.random()*arr.length), 1)[0].statement);
                 })
 
             })
             
         })
-        
-        // fetch("https://cors-anywhere.herokuapp.com/"+URL2)
-        // .then(results => {
-        //     return results.json();
-        // })
-        // .then(data => {
-        //     data=data.filter(function(item){return item.speaker.name_slug=="hillary-clinton" && item.statement_type.statement_type !== "Flip";});
-        //     for(var i=0; i<10; i++){
-        //         superArray.push(data.splice(Math.random()*data.length, 1)[0]);
-        //     }
-        // })
-        // fetch("https://cors-anywhere.herokuapp.com/"+URL3)
-        // .then(results => {
-        //     return results.json();
-        // })
-        // .then(data => {
-        //     data=data.filter(function(item){return item.speaker.name_slug=="bernie-s" && item.statement_type.statement_type !== "Flip";});
-        //     for(var i=0; i<10; i++){
-        //         superArray.push(data.splice(Math.random()*data.length, 1)[0]);
-        //     }
-        // })
-        // console.log(superArray);
-        // console.log(superArray[0]);
-        // this.setState({
-        //     statements: superArray,
-        //     claim1: superArray[Math.floor(Math.random()*superArray.length)],
-        // })
     }
+    //
     render(){
         return(
             <div className="container2">
                 <div className="container1">
-                    <div>{ReactHtmlParser(this.state.claim1)}</div>
+                    {/* {this.state.attempt} */}
+                    <TextField style={{ width: '30px', paddingLeft: '10px', backgroundColor: 'rgb(200, 200, 200)', borderRadius: '4px' }} value={this.state.testLength} onChange={(event) => {this.handleChange(event, 'testLength');}}/>
+                    <div>{this.state.claim1}</div>
+                    <div>{this.state.test}</div>
                 </div>
             </div>
         )
@@ -122,12 +137,20 @@ class Claim extends React.Component{
 class ChallengePage extends React.Component{
   state = {
     checked: true,
+    content: <button onClick={() => this.displayClaims()}>do the thing</button>
   };
 
   componentDidMount() {
     window.scrollTo(0,0);
   }
   
+  displayClaims(){
+      console.log("testing");
+      this.setState({
+          content: <Claim />
+      })
+  }
+
     render(){
       const font ={
         fontFamily: 'Raleway',
